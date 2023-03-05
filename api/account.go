9afe24210104964598a4dfa131d132e1ddbc8518
Guy Ronen <guy.ronen@codility.com>
2023-03-05 15:43:46 +0100

    delete update struct

--- api/account.go
+++ api/account.go
@@ -20,11 +20,6 @@ type deleteAccountRequest struct {
 	ID int64 `uri:"id" binding:"required"`
 }
 
-type updateAccountRequest struct {
-	ID      int64  `uri:"id" binding:"required"`
-	Balance int64  `json:"balance" binding:"required"`
-}
-
 type listAccountRequest struct {
 	PageID   int32 `form:"page_id" binding:"required,min=1"`
 	PageSize int32 `form:"page_size" binding:"required,min=1,max=10"`
